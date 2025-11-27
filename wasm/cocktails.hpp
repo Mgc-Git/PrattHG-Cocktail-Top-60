714848e0b4602f243e668e1da84146f6a168f9fb
Mgc-Git <markgchong@gmail.com>
2025-11-27 16:21:41 +1100

    cocktail list fixes

--- wasm/cocktails.hpp
+++ wasm/cocktails.hpp
@@ -129,7 +129,8 @@ push({"MISSIONARY’S DOWNFALL", {{"Havana Club 3 Y/O Anos",30},{"Peach Schnapps
       "none",0,"shaken","dump","crushed",{"mint sprig"},false});
 
 push({"DARK AND STORMY", {{"Goslings Black Seal Rum",45},{"Lime juice",15}},
-      "none",0,"build","na","regular",{"mint sprig"},false});
+      "none",0,"build","na","regular",{"mint sprig"},false}).mehtod_note =
+      "Layer dark rum on top as final step";
 
 push({"SINGAPORE SLING", {{"Beefeater Gin",45},{"Cherry Heering",15},{"Cointreau",7},{"Dom Benedictine",7},{"Pineapple juice",45},{"Lime juice",15},{"Grenadine",10}},
       "angostura bitters",1,"shaken","single","regular",{"cherry and orange slice","orange slice and cherry"},false});
